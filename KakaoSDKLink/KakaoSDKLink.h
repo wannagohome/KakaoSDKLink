e5804b9d0530fa97729d85a7c347318c0bf2cdf2
wannagohome <iwwuf7@gmail.com>
2020-09-24 15:56:12 +0900

    Initial Commit

---
+++ KakaoSDKLink/KakaoSDKLink.h
@@ -0,0 +1,19 @@
+//
+//  KakaoSDKLink.h
+//  KakaoSDKLink
+//
+//  Created by jinho jang on 2020/09/24.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KakaoSDKLink.
+FOUNDATION_EXPORT double KakaoSDKLinkVersionNumber;
+
+//! Project version string for KakaoSDKLink.
+FOUNDATION_EXPORT const unsigned char KakaoSDKLinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KakaoSDKLink/PublicHeader.h>
+
+
